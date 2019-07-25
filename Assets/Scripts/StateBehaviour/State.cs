45c318d638146a9e710f931881d69ec6121639b8
carlos <carloshlc@hotmail.com>
2019-07-25 13:53:40 -0300

    meu primeiro commit

---
+++ Assets/Scripts/StateBehaviour/State.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace StateBehaviour
+{
+    public interface IState
+    {
+        void Enter();
+        void Execute();
+        void Exit();
+    }
+
+    [SerializeField]
+    public abstract class State : IState
+    {
+        protected StateMachine _stateMachine;
+        public StateMachine StateMachine => _stateMachine;
+
+        public State(StateMachine stateMachine)
+        {
+            this._stateMachine = stateMachine;
+        }
+
+        public abstract void Enter();
+        public abstract void Execute();
+        public abstract void Exit();
+    }
+
+    public abstract class State<T> : State where T : MonoBehaviour
+    {
+        public new StateMachine<T> StateMachine => this._stateMachine as StateMachine<T>;
+
+        public State(StateMachine stateMachine) : base(stateMachine)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/StateBehaviour/StateMachine.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace StateBehaviour
+{
+    public interface IStateMachine
+    {
+        void UpdateStateMachine();
+        void ChangeState(State newState);
+    }
+
+    [SerializeField]
+    public abstract class StateMachine : IStateMachine
+    {
+        protected State _state;
+        public State State => _state;
+
+        protected MonoBehaviour _actor;
+       // public MonoBehaviour Actor => _actor;
+
+        public StateMachine(MonoBehaviour actor)
+        {
+            this._actor = actor;
+        }
+
+        public void ChangeState(State newState)
+        {
+            _state?.Exit();
+            _state = newState;
+            _state?.Enter();
+        }
+
+        public void UpdateStateMachine()
+        {
+            _state?.Execute();
+        }
+
+        public T GetActor<T>() where T : MonoBehaviour
+        {
+            return _actor as T;
+        }
+    }
+
+    public abstract class StateMachine<T> : StateMachine where T : MonoBehaviour
+    {
+        public new State<T> State => _state as State<T>;
+        
+        public StateMachine(MonoBehaviour actor) : base(actor)
+        {
+        }
+
+        public T GetActor()
+        {
+            return _actor as T;
+        }
+    }
+}
