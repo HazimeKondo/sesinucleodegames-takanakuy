0ab7cd9e4d869fb25d71de3f383326e81dc10b95
Hazime <hajime.lol@gmail.com>
2019-07-28 10:08:23 -0300

    Added some tools

---
+++ Assets/Tools/Fade/FadeEffect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace Fader
+{
+    using UnityEngine;
+
+    public class FadeEffect : MonoBehaviour
+    {
+        private Material _mat;
+        
+        private void Awake()
+        {
+            gameObject.AddComponent<MeshFilter>().mesh = Resources.GetBuiltinResource<Mesh>("Quad.fbx");
+            _mat = gameObject.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Unlit/AlwaysOnTop"));
+            _mat.color = Color.clear;
+            
+            Camera camera = Camera.main;
+            transform.SetParent(camera.transform);
+            
+            var nearClipPlane = camera.nearClipPlane;
+            float height = 2.0f * nearClipPlane * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            
+            transform.localPosition = Vector3.forward * (nearClipPlane+0.02f);
+            transform.localScale = new Vector3(height * camera.aspect,height,1)*1.5f;
+            transform.localRotation = Quaternion.identity;
+        }
+
+        public void StartFade(Color color, float duration)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeTo(color, duration));
+        }
+
+        private IEnumerator FadeTo(Color color, float duration)
+        {
+            Color originalColor = _mat.color;
+            float timer = 0;
+
+            while (timer <= duration)
+            {
+                timer += Time.deltaTime;
+                _mat.color = Color.Lerp(originalColor, color, timer / duration);
+                yield return null;
+            }
+
+            _mat.color = color;
+        }
+    }
+}
---
+++ Assets/Tools/OverlapCollider/BoxOverlapCollider.cs
@@ -0,0 +1,53 @@
+﻿namespace OverlapColliders
+{
+    using System.Linq;
+    using UnityEngine;
+    using System.Collections.Generic;
+    
+    public class BoxOverlapCollider : OverlapCollider
+    {
+        //Sizes of cube
+        public Vector3 Size = Vector3.one;
+
+        protected override List<Collider> Overlapping()
+        {
+            return Physics.OverlapBox(transform.position + GetOffset(), GetSize() / 2,
+                transform.rotation, LayerMaskToCollide).ToList();
+        }
+        
+        /// <summary>
+        /// Return the sizes with object scale applied
+        /// </summary>
+        public Vector3 GetSize()
+        {
+            Vector3 scaled;
+            scaled.x = Mathf.Abs(Size.x * transform.lossyScale.x);
+            scaled.y = Mathf.Abs(Size.y * transform.lossyScale.y);
+            scaled.z = Mathf.Abs(Size.z * transform.lossyScale.z);
+            return scaled;
+        }
+
+        #region DRAW GIZMOS
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (!this.enabled) return;
+            DrawSquare();
+        }
+
+        private void DrawSquare()
+        {
+            Matrix4x4 matrix = Gizmos.matrix;
+            Matrix4x4 trs = Matrix4x4.TRS(transform.position + GetOffset(), transform.rotation, GetSize());
+            Gizmos.matrix = trs;
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+            Gizmos.color = new Color(0f, 0f, 1f, 0.5f);
+            Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            Gizmos.matrix = matrix;
+        }
+#endif
+        #endregion
+    }
+}
