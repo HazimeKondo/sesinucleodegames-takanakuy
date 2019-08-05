0280ba9f8f758c3af386400e9e99e781bfae59d3
Chrystian Shiruo <chrystian.shiruo@gmail.com>
2019-08-05 04:32:18 -0300

    Ataque player
    "Mira" do player segue o cursor, opcoes de ataque instantaneo ou carregar alcance do ataque, indicadores provisorios de range e frente do player.

---
+++ Assets/Scripts/PlayerFacingDirection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerFacingDirection : MonoBehaviour {
+
+    private Camera cam;
+    public Vector3 point;
+    private GameObject go;
+    public Vector2 mousePosNormal;
+    private Vector2 screenDimensions;
+    private float goY;
+
+    private void Awake() {
+        go = new GameObject();
+        go.transform.position = transform.position;
+        cam = Camera.main;
+        screenDimensions = new Vector2(Screen.width, Screen.height);
+        goY = go.transform.localPosition.y;
+    }
+    private void Update() {
+
+        mousePosNormal = new Vector2(Input.mousePosition.x / screenDimensions.x, Input.mousePosition.y / screenDimensions.y);
+
+        Ray ray = cam.ViewportPointToRay(new Vector3(mousePosNormal.x, mousePosNormal.y, 0));
+        LayerMask mask = LayerMask.GetMask("MousePositionCapture");
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, mask.value)) {
+            point = hit.point;
+        }
+        point = new Vector3(point.x, goY, point.z);
+        transform.LookAt(point);
+    }
+}
--- Assets/Scripts/WhipBehaviour.cs
+++ Assets/Scripts/WhipBehaviour.cs
@@ -4,32 +4,65 @@ using OverlapColliders;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class WhipBehaviour : MonoBehaviour
-{
-    [SerializeField] private float _range = 2;
+public class WhipBehaviour : MonoBehaviour {
+    [SerializeField] private float _maxRange = 6f;
     [SerializeField] private float _halfThickness = 0.5f;
+    [SerializeField] private bool _isChargeAttack;
+    [SerializeField] private float _maxChargeTime = 2f;
+    [SerializeField] private GameObject _whip;
+    [SerializeField] private float _chargeTime;
+    private bool charge;
 
+    private void Start() {
+        Player.Input.Keyboard.Attack.performed += ctx => { Attack(); };
+        Player.Input.Keyboard.Attack.canceled += ctx => { EndCharge(); }; ;
 
-
-
-    private void Start()
-    {
-        Player.Input.Keyboard.Attack.performed += ctx => { Attack(); } ;
-    }
-    private void Update()
-    {
-        //rotacionar player
-    }
-
-    [Button]
-    private void Attack()
-    {        
-        RaycastHit hit;
-        if (Physics.SphereCast(transform.position + (transform.forward*transform.localScale.x/2), _halfThickness , Vector3.forward , out hit, _range))
-        {
-            hit.transform.GetComponent<CrowdBehaviour>().Hit();
+        if (!_isChargeAttack) {
+            _whip.transform.localPosition = (Vector3.forward * (_maxRange / 2f - transform.localScale.x / 2f));
+            _whip.transform.localScale = new Vector3(1f, 1f, _maxRange);
         }
     }
 
 
+    [Button]
+    private void Attack() {
+        if (!_isChargeAttack) {
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, _halfThickness,
+            _whip.transform.position - transform.position, out hit, transform.localScale.x / 2 + _maxRange)) {
+                if (hit.transform.GetComponent<CrowdBehaviour>() != null) {
+                    hit.transform.GetComponent<CrowdBehaviour>().Hit();
+                }
+            }
+
+        } else {
+            StartCoroutine(ChargeAttack());
+        }
+    }
+
+    private void EndCharge() {
+        charge = false;
+    }
+    private IEnumerator ChargeAttack() {
+        charge = true;
+        _chargeTime = 0;
+        while (charge) {
+            if (_chargeTime < _maxChargeTime) {
+                _chargeTime += Time.deltaTime;
+            }
+            if (_chargeTime > _maxChargeTime) {
+                _chargeTime = _maxChargeTime;
+            }
+            _whip.transform.localPosition = (Vector3.forward * (_maxRange * (_chargeTime / _maxChargeTime) / 2f - transform.localScale.x / 2f));
+            _whip.transform.localScale = new Vector3(1f, 1f, _maxRange * (_chargeTime / _maxChargeTime));
+
+            yield return null;
+        }
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, _halfThickness,
+            _whip.transform.position - transform.position, out hit, transform.localScale.x / 2 + _maxRange * (_chargeTime / _maxChargeTime))) {
+            hit.transform.GetComponent<CrowdBehaviour>().Hit();
+        }
+        _whip.transform.localScale = new Vector3(1f, 0f, 0f);
+    }
 }
