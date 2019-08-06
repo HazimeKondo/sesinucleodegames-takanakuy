a54b284108479daf12a322853acd92801570d42e
Carlos <carloshlc@hotmail.com>
2019-08-06 02:46:18 -0300

    tela de menu bem simples

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Credits()
+    {
+       // SceneManager.LoadScene("");
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
